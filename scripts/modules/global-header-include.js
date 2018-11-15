9077f8f4f561052819907415ebea08dbd1efaddf
sanjaySteg <ssingh1@stegtechnologies.com>
2018-11-15 12:10:13 +0530

    validation message for store locator input

--- scripts/modules/global-header-include.js
+++ scripts/modules/global-header-include.js
@@ -1,6 +1,6 @@
 define(
-    ['modules/jquery-mozu'],
-    function ($) {
+    ['modules/jquery-mozu', 'hyprlive'],
+    function ($,Hypr) {
         $(function () {
             $('#global-header-wrapper').each(function (index, globalHeader) {
                 globalHeader = $(globalHeader);
@@ -14,11 +14,17 @@ define(
                     sessionStorage.setItem('globalHeaderIncludeClosed', true);
                 });
 
-                $("#btnFindStore").click(function(e) {
+                 $("#btnFindStore").click(function(e) {
+                    console.log("btnFindStore");
                 e.preventDefault();
                 var zipcode = $.trim($("#footerZipCodeInput").val());
-                zipcode = (zipcode.length === 0 ? "Enter+Zip" : zipcode);
+                // zipcode = (zipcode.length === 0 ? "Enter+Zip" : zipcode);
+                if(zipcode.length > 4) {
+                    $('#zipcodeHelpBlock').text('');
                 window.location.href = window.location.origin + "/store-locator?zipcode=" + zipcode;
+                } else {
+                    $('#zipcodeHelpBlock').text(Hypr.getLabel('zipCodeEmpty'));
+                }
                 });
 
                 $("#footerZipCodeInput").keydown(function(e) {
