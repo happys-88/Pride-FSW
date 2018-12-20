ebb74850972c427df26fecc875ae5a12d0c88667
utsahpaikraySteg <upaikray@stegtechnologies.com>
2018-12-20 13:44:48 +0530

    prid-26

--- scripts/widgets/featured-products.js
+++ scripts/widgets/featured-products.js
@@ -3,41 +3,46 @@ define([
     'modules/jquery-mozu',
     'slick'
 ],
-    function ($, bxSlider, slick) {
+    function ($, slick) {
         var slide = {
             productCarousel: function () { 
                 $('.mz-featured-products').show();
                 $(".mz-featured-products .mz-productlist-list").slick({
                     infinite: false,
                     slidesToShow: 5,
+                    dots: true,
                     prevArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>',
                     nextArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>',
                     responsive: [{
                         breakpoint: 1024,
                         settings: {
                             arrows: true,
-                            slidesToShow: 5
+                            slidesToShow: 5,
+                            dots: false
                         }
                     },
                     {
                         breakpoint: 992,
                         settings: {
                             arrows: true,
-                            slidesToShow: 4
+                            slidesToShow: 4,
+                            dots: false
                         }
                     },
                     {
                         breakpoint: 768,
                         settings: {
-                            arrows: true,
-                            slidesToShow: 3
+                            arrows: false,
+                            slidesToShow: 3,
+                            dots: true
                         }
                     },
                     {
                         breakpoint: 460,
                         settings: {
-                            arrows: true,
-                            slidesToShow: 2
+                            arrows: false,
+                            slidesToShow: 2,
+                            dots: true
                         }
                     }
 
