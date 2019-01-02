c6852742afb52b271a5995099b41996c7a4a0a21
singh-vsc <vsingh@stegtechnologies.com>
2019-01-02 14:42:04 +0530

    PRID-33

--- scripts/pages/confirmation.js
+++ scripts/pages/confirmation.js
@@ -15,6 +15,65 @@ define(['modules/api',
           include an array of locationDetails.
           */
 
+          var SignupGuest = Backbone.MozuView.extend({
+            templateName: "modules/checkout/confirmation-signup",
+            additionalEvents: {
+              "click [data-mz-value=submitAccount]":"signup"
+          },
+          validate: function (payload) {
+            var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            if(!filter.test(payload.account.emailAddress)) return this.displayMessage(Hypr.getLabel('emailMissing')), false;
+            if (!payload.account.emailAddress) return this.displayMessage(Hypr.getLabel('emailMissing')), false;
+                  if (!payload.password) return this.displayMessage(Hypr.getLabel('passwordMissing')), false;
+                  if (payload.password !== $('#confirmPasswordSignUp').val()) return this.displayMessage(Hypr.getLabel('passwordsDoNotMatch')), false;
+                  return true;
+              },
+                displayMessage: function (msg) {
+                  // this.setLoading("false");
+                    $('#errorMessageSignUp').html('<span class="mz-validationmessage">' + msg + '</span>');
+              },
+                displayApiMessage: function (xhr) {
+                    if(typeof xhr.message !== 'undefined' && xhr.message !== '') {
+                        var errorMsg = xhr.message;
+                        if(errorMsg.toLowerCase().indexOf('missing') > -1 ) {
+                            var val = errorMsg.split(':');
+                            errorMsg = val[1].trim();
+                            errorMsg = errorMsg.substr(errorMsg.indexOf(' ')+1, errorMsg.length);
+                        }
+                        $('#errorMessageSignUp').html('<span class="mz-validationmessage">' + errorMsg + '</span>');
+                    }
+                },
+          signup: function () {
+                var self = this,
+                    email = $('#emailAddressSignup').val(),
+                    firstName = '',
+                    lastName = '',
+                    password = $('#passwordSignup').val(),
+                    payload = {
+                        account: {
+                            emailAddress: email,
+                            userName: email,
+                            firstName: firstName,
+                            lastName: lastName,
+                            contacts: [{
+                                email: email,
+                                firstName: firstName,
+                                lastNameOrSurname: lastName
+                            }]
+                        },
+                        password: $('#passwordSignup').val()
+                    };
+                if (this.validate(payload)) {   
+                  //var user = api.createSync('user', payload);
+                    // this.setLoading(true);
+                    return api.action('customer', 'createStorefront', payload).then(function () {
+                        $('#createAccountSignUp').hide();
+                        $('#accountCreatedSignUp').show();
+                    }, self.displayApiMessage);
+                }
+            } 
+        });
+
           var ConfirmationView = Backbone.MozuView.extend({
             templateName: 'modules/confirmation/confirmation-detail',
             events: {
@@ -51,6 +110,16 @@ define(['modules/api',
         });
 
           $(document).ready(function(){
+
+            var orderData = require.mozuData('order');
+            var orderModel = Backbone.Model.extend(); 
+            var order = new orderModel(orderData);
+            var view = new SignupGuest({
+              model: order,
+              el: $('#guestUserSignUp')
+            });
+            view.render();
+            
             var confModel = ConfirmationModel.fromCurrent();
             confModel.getLocationData().then(function(response){
               confModel.set('locationDetails', response.data.items);
