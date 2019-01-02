c1dda2da913e1646ec7c2a09a292f4d6e766fbe6
happys-88 <hmasih@stegtechnologies.com>
2019-01-02 15:20:17 +0530

    Fixed PRID-41 & 45

--- scripts/modules/login-links.js
+++ scripts/modules/login-links.js
@@ -157,7 +157,7 @@ define(['shim!vendor/bootstrap/js/popover[shim!vendor/bootstrap/js/tooltip[modul
     LoginPopover.prototype = new DismissablePopover();
     $.extend(LoginPopover.prototype, {
         boundMethods: ['handleEnterKey', 'handleLoginComplete', 'displayResetPasswordMessage', 'dismisser', 'displayMessage', 'displayApiMessage', 'createPopover', 'slideRight', 'slideLeft', 'login', 'retrievePassword', 'onPopoverShow'],
-        template: Hypr.getTemplate('modules/common/login-popover').render(),
+        //template: Hypr.getTemplate('modules/common/login-popover').render(), 
         bindListeners: function (on) {
             var onOrOff = on ? "on" : "off";
             this.$parent[onOrOff]('click', '[data-mz-action="forgotpasswordform"]', this.slideRight);
