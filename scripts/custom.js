d365fb4a7533c23f12adbb461b81ac87ee8b2371
ashish-steg <akumar3@stegtechnologies.com>
2019-01-21 19:10:42 +0530

    Worked on PRID-66

--- scripts/custom.js
+++ scripts/custom.js
@@ -126,7 +126,7 @@ define([
 		$("#back-to-top").click(function(){
 	    scrollToTop();  
 		});
-    $('.nav-tabs li:eq(0)').tab('show');
+    $('.nav-tabs li:eq(0) a').tab('show');
 	}); 
 
   $("#resetPassword").blur(function() { 
